94c619e023e67bd274fd7a5996082d2ee4c5c297
YanaKomarova <yana86komarova@gmail.com>
2023-08-31 19:41:56 +0300

    1 задача

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,35 @@
-"use strict"
+“use strict”
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+let arr = [];
+
+return arr;
+let arr = [];
+let d = b ** 2 - 4 * a * c;
+
+if (d < 0) {
+arr = [];
+console.log(‘Коэффициент не может быть равен нулю’);
+} else if (d === 0) {
+
+1
+2
+3
+let x1 = -b / (2 * a);
+
+arr.push(x1);
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+if (d > 0) {
+
+1
+2
+3
+4
+let x1 = (-b + Math.sqrt(d)) / (2 * a);
+let x2 = (-b - Math.sqrt(d)) / (2 * a);
+
+arr.push(x1, x2);
 }
+
+return arr;
