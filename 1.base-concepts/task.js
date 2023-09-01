6436b879c7c0859278f1fee2dcf6d80fa044fdd2
YanaKomarova <yana86komarova@gmail.com>
2023-09-01 17:30:37 +0300

    Первая задача готова

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,35 +1,19 @@
-“use strict”
+"use strict";
 
 function solveEquation(a, b, c) {
-let arr = [];
+  let d = b ** 2 - 4 * a * c;
+  let arr = [];
 
-return arr;
-let arr = [];
-let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    let arr = -b / (2 * a);
+    arr.push(x1);
+  } else {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+  }
 
-if (d < 0) {
-arr = [];
-console.log(‘Коэффициент не может быть равен нулю’);
-} else if (d === 0) {
-
-1
-2
-3
-let x1 = -b / (2 * a);
-
-arr.push(x1);
+  return arr;
 }
-
-if (d > 0) {
-
-1
-2
-3
-4
-let x1 = (-b + Math.sqrt(d)) / (2 * a);
-let x2 = (-b - Math.sqrt(d)) / (2 * a);
-
-arr.push(x1, x2);
-}
-
-return arr;
