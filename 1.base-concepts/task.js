dda8a39bfc1b5fbe1b6b9dac01a315466a739d3b
YanaKomarova <yana86komarova@gmail.com>
2023-09-06 15:07:31 +0300

    ДЗ 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,19 +1,37 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-  let d = b ** 2 - 4 * a * c;
-  let arr = [];
-
+  let arr = [];  
+  let d = (b  2) - (4 * a * c);
   if (d < 0) {
-    return arr;
-  } else if (d === 0) {
-    let arr = -b / (2 * a);
-    arr.push(x1);
-  } else {
-    let x1 = (-b + Math.sqrt(d)) / (2 * a);
-    let x2 = (-b - Math.sqrt(d)) / (2 * a);
-    arr.push(x1, x2);
-  }
+    arr.push();
+  } else if (d == 0) {    
+    let oneRoot = -b / (2 * a);
+      arr.push(oneRoot);
+  } else if (d > 0) {
+    let firstRoot = (-b + Math.sqrt(d))/(2 * a);
+    let secondRoot = (-b - Math.sqrt(d))/(2 * a);
+      arr.push(firstRoot, secondRoot);          
+  } 
+  return arr; 
+}
 
-  return arr;
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+   let totalAmount;
+    if (isNaN(percent)) {
+    return totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (isNaN(contribution)) {
+    return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (isNaN(amount)) {
+    return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  }   
+  let returnAmountBank = amount - contribution;  
+  let date1 = new Date();  
+  let diff = Date.parse(date) - Date.parse(date1);
+  let creditTerm = Math.ceil(diff / 1000 / 60 / 60 / 24 / 30.5);  
+  let P = percent / 12 / 100;
+  let monthlyFee = returnAmountBank * (P + (P / (((1 + P)  creditTerm) - 1)));
+      totalAmount = creditTerm * monthlyFee;        
+    return Number(totalAmount.toFixed(2));  
 }
