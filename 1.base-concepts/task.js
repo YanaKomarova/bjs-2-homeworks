7263d7261f22344c773730f1167d0966c8e18c52
YanaKomarova <yana86komarova@gmail.com>
2023-09-19 22:56:04 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,37 +1,28 @@
 "use strict";
 
-function solveEquation(a, b, c) {
-  let arr = [];  
-  let d = (b  2) - (4 * a * c);
-  if (d < 0) {
-    arr.push();
-  } else if (d == 0) {    
-    let oneRoot = -b / (2 * a);
-      arr.push(oneRoot);
-  } else if (d > 0) {
-    let firstRoot = (-b + Math.sqrt(d))/(2 * a);
-    let secondRoot = (-b - Math.sqrt(d))/(2 * a);
-      arr.push(firstRoot, secondRoot);          
-  } 
-  return arr; 
+function solveEquation(a, b, c) { // реализуем функцию
+
+  let arr = []; // создаём массив
+  let d = Math.pow(b, 2) - (4 * a * c); // считаем d = дискриминант квадратного уравнения
+  let f = 2 * a;
+  if (d == 0) { //КОГДА 0=дискриминант, ТОГДА корень 1шт
+    arr.push(-b / f); //Пушим значение корня в массив arr
+  } else if (d > 0) { //А КОГДА >0 ТОГДА корень 2шт
+    let e = Math.sqrt(d);
+    arr.push((-b + e) / f, (-b - e) / f);
+  }
+  return arr; // возвращаем массив 
 }
 
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-   let totalAmount;
-    if (isNaN(percent)) {
-    return totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
-  } else if (isNaN(contribution)) {
-    return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
-  } else if (isNaN(amount)) {
-    return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
-  }   
-  let returnAmountBank = amount - contribution;  
-  let date1 = new Date();  
-  let diff = Date.parse(date) - Date.parse(date1);
-  let creditTerm = Math.ceil(diff / 1000 / 60 / 60 / 24 / 30.5);  
-  let P = percent / 12 / 100;
-  let monthlyFee = returnAmountBank * (P + (P / (((1 + P)  creditTerm) - 1)));
-      totalAmount = creditTerm * monthlyFee;        
-    return Number(totalAmount.toFixed(2));  
+
+
+
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) { //реализуем функцию 
+  let newPercent = percent / 100; //Преобразуем процентную ставку из диапазона от 0 до 100 
+  let monthsPercent = newPercent / 12; //  в диапазон от 0 до 1 и из годовой ставки — в месячную.
+  let bodyCredit = amount - contribution; //Высчитываем тело кредита 
+  let onlyPayment = bodyCredit * (monthsPercent + (monthsPercent / ((Math.pow((1 + monthsPercent), countMonths)) - 1))); // расчёт по формуле                                      
+  let totalPayment = Math.round(totalPayment * 100.0) / 100.0; //Считаем общую сумму, которую придётся заплатить клиенту. Округляем результат до двух значений после запятой. Возвращаем результат из функции. 
 }
