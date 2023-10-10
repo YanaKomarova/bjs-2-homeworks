9145e78580126c68037720cd91177ea7cf4562d5
YanaKomarova <yana86komarova@gmail.com>
2023-10-10 20:42:23 +0300

    Добавлена 1 задача

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,44 @@
+// Создаём функцию-конструктор Student
 function Student(name, gender, age) {
-  
-}
+	// Инициализация свойств объекта Student
+	this.name = name; // Имя студента
+	this.gender = gender; // Пол студента
+	this.age = age; // Возраст студента
+	this.marks = []; // Создаём пустой массив для хранения оценок
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+	// Метод для установки предмета
+	this.setSubject = function(subjectName) {
+		this.subject = subjectName; // Устанавливаем значение свойства subject равным значению аргумента subjectName
+	};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+	// Метод для добавления оценок
+	this.addMarks = function(...marksToAdd) {
+		// Добавляем оценки из массива marksToAdd в массив оценок marks, если он существует
+		if (this.marks) {
+			this.marks.push(...marksToAdd);
+		}
+	};
 
-Student.prototype.getAverage = function () {
-  
-}
+	// Метод для расчета среднего значения оценок
+	this.getAverage = function() {
+		// Если массив оценок пуст или отсутствует, возвращаем 0
+		if (!this.marks || this.marks.length === 0) {
+			return 0;
+		}
 
-Student.prototype.exclude = function (reason) {
-  
+		// Вычисляем сумму оценок
+		const sum = this.marks.reduce((total, mark) => total + mark, 0);
+		// Возвращаем среднее значение оценок, деление суммы на количество
+		return sum / this.marks.length;
+	};
+
+	// Метод для исключения студента 
+	this.exclude = function(reason) {
+		// Удаление свойств subject и marks
+		delete this.subject;
+		delete this.marks;
+
+		// Добавление свойства excluded, равного причине исключения
+		this.excluded = reason;
+	};
 }
